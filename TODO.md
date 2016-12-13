dc7a1715808d8b0d21ae3d5c0bcec913deb03960
Tom <tom.marx@hotmail.fr>
2016-12-13 19:10:20 +0100

    Initial commit

---
+++ TODO.md
@@ -0,0 +1,25 @@
+# TODO
+
+## Game structure
+- [ ] Game class
+- [ ] Screen class
+- [ ] Drawable : render(), dispose()
+
+## Game object
+- [ ] Abstract game object class
+- [ ] UID
+- [ ] Tag
+- [ ] Get component by class
+- [ ] Get component by tag
+
+## Screen
+- [ ] Add game object
+- [ ] Get game object by tag
+- [ ] Remove game object by tag
+
+## Component
+- [ ] Offsets
+- [ ] Render()
+- [ ] Tag
+
+## Utils
---
+++ desktop/src/fr/tommarx/gameengine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.tommarx.gameengine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.tommarx.gameengine.GameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameClass(), config);
+	}
+}

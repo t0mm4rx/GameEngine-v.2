72f278e840eaf235d97c113a555f339e8d4bdb58
Tom <tom.marx@hotmail.fr>
2016-12-16 23:04:47 +0100

    Circle body

--- core/src/fr/tommarx/gameengine/Components/CircleBody.java
+++ core/src/fr/tommarx/gameengine/Components/CircleBody.java
@@ -1,85 +1,25 @@
 package fr.tommarx.gameengine.Components;
 
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 
-import fr.tommarx.gameengine.Game.Game;
 import fr.tommarx.gameengine.Game.AbstractGameObject;
-import fr.tommarx.gameengine.Util.Math;
 
-public class CircleBody extends Component{
+public class CircleBody extends Body{
 
-    private com.badlogic.gdx.physics.box2d.Body body;
-    private float radius;
-
-    public CircleBody(AbstractGameObject go, BodyDef.BodyType bodyType) {
+    public CircleBody(AbstractGameObject go, float radius, BodyDef.BodyType bodyType, boolean isSensor) {
         super(go);
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = bodyType;
-        bodyDef.position.set(go.getTransform().getPosition().x, go.getTransform().getPosition().y);
-        bodyDef.angle = Math.DegreeToRadian(go.getTransform().getRotation());
-        body = Game.getCurrentScreen().world.createBody(bodyDef);
-        CircleShape shape = new CircleShape();
-        if (go.getSpriteRenderer() != null) {
-            shape.setRadius(go.getSpriteRenderer().getTexture().getWidth() / 2 * go.getTransform().getScale().x);
-            radius = go.getSpriteRenderer().getTexture().getWidth() / 2 * go.getTransform().getScale().x;
-        } else {
-            System.err.println("Warning ! The given gameobject has no sprite renderer, please use CircleBody(AbstractGameObject go, float radius, BodyDef.BodyType bodyType) constructor.");
-            shape.setRadius(10);
-            radius = 10;
-        }
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = shape;
-        fixtureDef.density = 1f;
-        body.createFixture(fixtureDef);
-        shape.dispose();
-    }
-
-    public CircleBody(AbstractGameObject go, float radius, BodyDef.BodyType bodyType) {
-        super(go);
-        this.radius = radius;
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = bodyType;
-        bodyDef.position.set(go.getTransform().getPosition().x, go.getTransform().getPosition().y);
-        bodyDef.angle = Math.DegreeToRadian(go.getTransform().getRotation());
-        body = Game.getCurrentScreen().world.createBody(bodyDef);
         CircleShape shape = new CircleShape();
         shape.setRadius(radius);
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
         fixtureDef.density = 1f;
-        body.createFixture(fixtureDef);
-        shape.dispose();
-    }
-
-    public void render() {
-
-    }
-
-    public void renderInHUD() {
-
-    }
-
-    public void update() {
-        getGameObject().getTransform().setPosition(body.getPosition());
-        getGameObject().getTransform().setRotation(Math.RadianToDegree(body.getAngle()));
-    }
-
-    public void dispose() {
-        if (body != null) {
-            body.getWorld().destroyBody(body);
-            body.setUserData(null);
-            body = null;
+        if (isSensor) {
+            fixtureDef.isSensor = true;
         }
-    }
-
-    public float getRadius() {
-        return radius;
-    }
-    public Body getBody() {
-        return body;
+        initBody(bodyType, fixtureDef);
+        shape.dispose();
     }
 
 }
--- core/src/fr/tommarx/gameenginetest/Coin.java
+++ core/src/fr/tommarx/gameenginetest/Coin.java
@@ -1,8 +1,10 @@
 package fr.tommarx.gameenginetest;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 
 import fr.tommarx.gameengine.Components.BoxRenderer;
+import fr.tommarx.gameengine.Components.CircleBody;
 import fr.tommarx.gameengine.Components.Transform;
 import fr.tommarx.gameengine.Game.AbstractGameObject;
 
@@ -11,6 +13,7 @@ public class Coin extends AbstractGameObject{
     public Coin(Transform transform) {
         super(transform);
         addComponent(new BoxRenderer(this, 16, 16, Color.YELLOW));
+        addComponent(new CircleBody(this, 8, BodyDef.BodyType.DynamicBody, false));
     }
 
     protected void update(float delta) {

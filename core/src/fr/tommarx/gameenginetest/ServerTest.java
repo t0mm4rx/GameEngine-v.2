9d55a9ce456a6c0472b494accfe517d9652ed6c1
Tom <tom.marx@hotmail.fr>
2017-06-01 18:21:32 +0200

    Delete skeleton

---
+++ core/src/fr/tommarx/gameenginetest/ServerTest.java
@@ -0,0 +1,27 @@
+package fr.tommarx.gameenginetest;
+
+import fr.tommarx.gameengine.Game.Game;
+import fr.tommarx.gameengine.Game.Screen;
+
+public class ServerTest extends Screen{
+
+    public ServerTest(Game game) {
+        super(game);
+    }
+
+    public void renderBefore() {
+
+    }
+
+    public void renderAfter() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void show() {
+
+    }
+}
